497e52a5c6354c085a628fa27c548226ed296d04
bakmaiev <thisyoungrhyme@gmail.com>
2023-04-04 20:08:18 +0300

    Add second variant with scroll

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 'use strict';
 import Notiflix from 'notiflix';
+import throttle from 'lodash.throttle';
 import SimpleLightbox from 'simplelightbox';
 import InfiniteScroll from 'infinite-scroll';
 import 'simplelightbox/dist/simple-lightbox.min.css';
@@ -17,12 +18,14 @@ loadMoreBtnEl.classList.add('visually-hidden');
 
 const pixabaiAPI = new PixabaiAPI();
 
+// --- First variant without infinit scrool ---
+
 const handleSearchFormEl = async e => {
   e.preventDefault();
   resetGalleryMarkup();
 
   const seekedCard = e.target.elements['searchQuery'].value.trim();
-  pixabaiAPI.query = seekedCard;
+  pixabaiAPI.query = seekedCard.trim();
   pixabaiAPI.resetPage();
 
   if (!seekedCard) {
@@ -37,7 +40,7 @@ const handleSearchFormEl = async e => {
         'Sorry, there are no images matching your search query. Please try again.'
       );
     }
-    galleryListEl.innerHTML = createMarkup(data.hits);
+    galleryListEl.insertAdjacentHTML('beforeend', createMarkup(data.hits));
     loadMoreBtnEl.classList.remove('visually-hidden');
     pixabaiAPI.setTotal(data.totalHits);
     lightbox.refresh();
@@ -96,3 +99,68 @@ const pageScrolling = () => {
 
 searchFormEl.addEventListener('submit', handleSearchFormEl);
 loadMoreBtnEl.addEventListener('click', handleloadMoreBtnEl);
+
+// --- Second variant with infinit scroll ---
+
+// const handleSearchFormEl = e => {
+//   e.preventDefault();
+//   resetGalleryMarkup();
+
+//   const seekedCard = searchFormEl.elements['searchQuery'].value.trim();
+//   pixabaiAPI.query = seekedCard;
+//   pixabaiAPI.resetPage();
+
+//   if (!seekedCard) {
+//     return Notiflix.Notify.failure('Enter something.');
+//   }
+
+//   fetchImages();
+// };
+
+// const fetchImages = async () => {
+//   try {
+//     const { data } = await pixabaiAPI.fetchCard();
+//     if (!data.total) {
+//       return Notiflix.Notify.failure(
+//         'Sorry, there are no images matching your search query. Please try again.'
+//       );
+//     }
+//     if (pixabaiAPI.page === 1) {
+//       Notiflix.Notify.success(`Done! We found ${data.totalHits} images.`);
+//     } else if (!pixabaiAPI.hasMoreImages()) {
+//       Notiflix.Notify.info(
+//         "We're sorry, but you've reached the end of search results."
+//       );
+//     }
+
+//     galleryListEl.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+//     pixabaiAPI.setTotal(data.totalHits);
+//     pixabaiAPI.incrementPage();
+//     lightbox.refresh();
+//   } catch (err) {
+//     console.log;
+//   }
+// };
+
+// const resetGalleryMarkup = () => {
+//   galleryListEl.innerHTML = '';
+// };
+
+// async function checkPosition() {
+//   const height = document.body.offsetHeight;
+//   const screenHeight = window.innerHeight;
+//   const scrolled = window.scrollY;
+//   const threshold = height - screenHeight / 4;
+//   const position = scrolled + screenHeight;
+
+//   if (position >= threshold) {
+//     fetchImages();
+//   }
+// }
+
+// searchFormEl.addEventListener('submit', handleSearchFormEl);
+
+// (() => {
+//   window.addEventListener('scroll', throttle(checkPosition, 250));
+//   window.addEventListener('resize', throttle(checkPosition, 250));
+// })();
